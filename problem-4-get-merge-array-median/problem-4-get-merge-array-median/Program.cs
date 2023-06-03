338ee6e73c4c97c462e6ddcc67681d017addbd0d
hackbabu <hack870629@gmail.com>
2023-06-03 19:39:17 +0800

    a

--- problem-4-get-merge-array-median/problem-4-get-merge-array-median/Program.cs
+++ problem-4-get-merge-array-median/problem-4-get-merge-array-median/Program.cs
@@ -9,32 +9,32 @@ namespace problem_4_get_merge_array_median
         {
             var num1 = new int[] { 3, 17, 35, 49 };
             var nums2 = new int[] { 3, 8, 19, 21, 27, 35 };
-            FindMedianSortedArrays(num1, nums2);
+            var result = FindMedianSortedArrays(num1, nums2);
         }
 
-        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
             var m = nums1.Length;
             var n = nums2.Length;
 
-            var leftIndex = 0;
-            var rightIndex = m + n - 1;
-            var leftMax = 0;
-            var rightMin = 0;
+            var mergedArrayLeftIndex = 0;
+            var mergedArrayRightIndex = m + n - 1;
+            var mergedArrayLeftMax = 0;
+            var mergeArrayRightMin = 0;
             var num1left = 0;
             var num1right = n;
             var num2left = 0;
             var num2right = m;
-            
+
 
             double nums1Median;
             double nums2Median;
-            while (rightIndex - leftIndex <= 1)
+            while (mergedArrayRightIndex - mergedArrayLeftIndex <= 1)
             {
 
                 if (num1left == num1right)
                 {
-                    
+
                 }
                 if (num2left == num2right)
                 {
@@ -44,12 +44,24 @@ namespace problem_4_get_merge_array_median
                 nums2Median = ComputeMedian(nums2, num2left, num2right);
                 if (nums1Median > nums2Median)
                 {
-                    num1right = n / 2;
-                    rightIndex = rightIndex - n / 2 + 1;
-                    rightMin = nums1[n / 2 + 1];
+                    // n/2改成用leftIndex與rightIndex決定
+                    num1right = (num1right - num1left) / 2;
+                    mergedArrayRightIndex = mergedArrayRightIndex - (num1right - num1left) / 2 + 1;
+                    mergeArrayRightMin = nums1[(num1right - num1left) / 2 + 1];
+
+                    num2left = (num2right - num2left) / 2;
+                    mergedArrayLeftIndex = mergedArrayLeftIndex + (num2right - num2left) / 2 + 1;
+                    mergedArrayLeftMax = nums2[(num2right - num2left) / 2];
                 }
                 else if (nums1Median < nums2Median)
                 {
+                    num1left = (num1right - num1left) / 2;
+                    mergedArrayLeftIndex = mergedArrayLeftMax + (num1right - num1left) / 2 + 1;
+                    mergedArrayLeftMax = nums1[(num1right - num1left) / 2];
+
+                    num2right = (num2right - num2left) / 2;
+                    mergedArrayRightIndex = mergedArrayRightIndex - (num2right - num2left) / 2 + 1;
+                    mergeArrayRightMin = nums2[(num2right - num2left) / 2];
                     
                 }
                 else
@@ -58,8 +70,10 @@ namespace problem_4_get_merge_array_median
                     return nums1Median;
                 }
             }
+
+            return 0;
         }
-        public double ComputeMedian(int[] array, int left, int right)
+        public static double ComputeMedian(int[] array, int left, int right)
         {
             var middle = (left + right) / 2;
             if (middle % 2 == 0)
